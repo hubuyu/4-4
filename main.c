d0a1395bb0936e9e0c2b00daccd3861f88822654
hubuyu <tuanhe@126.com>
2014-12-23 13:07:43 +0800

    Create main.c

---
+++ main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <string.h>
+
+#define DEV_NAME	"/dev/hubuyu"
+
+int main()
+{	
+	int fd ;
+	int err ;
+	char data[6];
+	char *send_buf="RaspberryPI";
+	fd = open(DEV_NAME,O_RDWR);
+	if(fd<0)
+	{
+		printf("Open %s failed \n" ,DEV_NAME );	
+		return -1 ;
+	}
+	err = write(fd , send_buf , strlen(send_buf));
+	if(err<0){
+		printf("Write data to kernel failed\n");
+		return -1 ;
+	}
+	err = read(fd , data ,sizeof(data));
+	printf("read length is %d \n", sizeof(data));
+	if(err<0){
+		printf("Read data from kernel failed\n");
+		return -1 ;
+	}
+	printf("Read data-%s \n",data);
+	close(fd);
+}
